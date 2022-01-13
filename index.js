db2724f11ad93280f4de0e97afef9fefb06fa994
sakkarakaya <sakkarakaya@icloud.com>
2022-01-13 13:18:28 +0100

    added helmet

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ const express = require("express")
 const dotenv = require("dotenv")
 const mongoose = require("mongoose")
 
+const helmet = require("helmet")
+
 const app = express()
 dotenv.config()
 mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
     console.log("Connected to MongoDB")
 })
+
+app.use(helmet())
 
 
 const port = 5000
