9abb2689827ee8b889fc6556c1a568e31427cc65
sakkarakaya <sakkarakaya@icloud.com>
2022-01-13 13:41:19 +0100

    added routers

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const mongoose = require("mongoose")
 const helmet = require("helmet")
 const morgan = require("morgan")
 
+const userRoute = require("./routers/users")
+const authRoute = require("./routers/auth")
+
 const app = express()
 dotenv.config()
 mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
@@ -15,16 +18,15 @@ app.use(express.json())
 app.use(helmet())
 app.use(morgan("common"))
 
-
-const port = 5000
+app.use("/api/users", userRoute)
+app.use("/api/auth", authRoute)
 
 app.get('/', (req, res) => {
     console.log("hi")
-    res.send("hhh")
+    res.send("Home")
 })
 
-
-
+const port = 5000
 app.listen(port, () => {
     console.log(`listening http://localhost:${port}`)
 })
--- routers/users.js
+++ routers/users.js
@@ -1,7 +1,7 @@
 const router = require("express").Router()
 
 router.get("/", (req, res) => {
-    res.send("Auth")
+    res.send("Users")
 })
 
 module.exports = router
