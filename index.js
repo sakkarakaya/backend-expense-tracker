6936050e15d09fed9c5d42d5e915f9a878389f63
sakkarakaya <sakkarakaya@icloud.com>
2022-01-13 13:01:09 +0100

    added mongoose

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express")
 const dotenv = require("dotenv")
+const mongoose = require("mongoose")
 
 const app = express()
 dotenv.config()
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
+    console.log("Connected to MongoDB")
+})
 
 
 const port = 5000
