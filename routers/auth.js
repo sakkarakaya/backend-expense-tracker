177e1dda6c03db99b8cd3f74ef1cd42c968e13d9
sakkarakaya <sakkarakaya@icloud.com>
2022-01-15 13:47:46 +0100

    added login

--- routers/auth.js
+++ routers/auth.js
@@ -1,17 +1,32 @@
 const router = require("express").Router()
 const User = require("../models/User.js")
+const bcrypt = require("bcrypt")
 
 router.post("/register", async (req, res) => {
-    const newUser = await new User({
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password
-    })
     try {
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(req.body.password, salt)
+        const newUser = new User({
+            username: req.body.username,
+            email: req.body.email,
+            password: hashedPassword
+        })
         const user = await newUser.save()
         res.status(200).json(user)
     } catch (error) {
-        console.log(error)
+        res.status(500).json(error)
+    }
+})
+
+router.post("/login", async (req, res) => {
+    try {
+        const user = await User.findOne({ email: req.body.email })
+        !user && res.status(404).json("email not found")
+        const password = await bcrypt.compare(req.body.password, user.password)
+        !password && res.status(400).json("wrong password")
+        res.status(200).json(user)
+    } catch (error) {
+        res.status(500).json(error)
     }
 })
 
